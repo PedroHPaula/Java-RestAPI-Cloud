1871de69aa86585e826e59f06db397b18683575b
Pedro Henrique de Paula <pedrohpaula98@gmail.com>
2023-10-02 20:31:09 -0400

    Initial commit

---
+++ src/test/java/php/RestAPICloud/RestApiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package php.RestAPICloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
